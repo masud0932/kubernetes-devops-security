51093bd32e9b03723c6c5813170ae8226ad57ccd
masud0932 <153615670+masud0932@users.noreply.github.com>
2025-07-02 10:43:19 +0200

    Refactor Compare class in NumericController for improved readability and structure

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -40,8 +40,8 @@ public class NumericController {
 		public int increment(@PathVariable int value) {
 			ResponseEntity<String> responseEntity = restTemplate.getForEntity(baseURL + '/' + value, String.class);
 			String response = responseEntity.getBody();
-			logger.info("Value Received in Request - {}" value);
-			logger.info("Node Service Response - {}" response);
+			logger.info("Value Received in Request - {}", value);
+			logger.info("Node Service Response - {}", response);
 			return Integer.parseInt(response);
 		}
 	}
