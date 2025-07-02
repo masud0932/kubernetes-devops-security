2fc9cc57014ca62c8c3508b35413088611a838ba
masud0932 <153615670+masud0932@users.noreply.github.com>
2025-07-02 10:38:09 +0200

    Fix constant naming convention and class/method casing in NumericController for consistency

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -13,20 +13,20 @@ import org.springframework.web.client.RestTemplate;
 public class NumericController {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
-	private static final String baseURL = "http://node-service:5000/plusone";
+	private static final String BASE_URL = "http://node-service:5000/plusone";
 	
 	RestTemplate restTemplate = new RestTemplate();
 	
 	@RestController
-	public class compare {
+	public class Compare {
 
 		@GetMapping("/")
 		public String welcome() {
 			return "Kubernetes DevSecOps";
 		}
 
-		@GetMapping("/compare/{value}")
-		public String compareToFifty(@PathVariable int value) {
+		@GetMapping("/Compare/{value}")
+		public String CompareToFifty(@PathVariable int value) {
 			String message = "Could not determine comparison";
 			if (value > 50) {
 				message = "Greater than 50";
@@ -40,8 +40,8 @@ public class NumericController {
 		public int increment(@PathVariable int value) {
 			ResponseEntity<String> responseEntity = restTemplate.getForEntity(baseURL + '/' + value, String.class);
 			String response = responseEntity.getBody();
-			logger.info("Value Received in Request - " + value);
-			logger.info("Node Service Response - " + response);
+			logger.info("Value Received in Request - {}" value);
+			logger.info("Node Service Response - {}" response);
 			return Integer.parseInt(response);
 		}
 	}
