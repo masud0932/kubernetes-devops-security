c66d64dbf19d47c3bb2d2ec5a1fdd1a8a6331935
masud0932 <153615670+masud0932@users.noreply.github.com>
2025-07-03 18:37:12 +0200

    Remove commented-out deployment existence check from k8s-deployment.sh for clarity

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -2,15 +2,17 @@
 
 #k8s-deployment.sh
 
-kubectl -n default get deployment devsecops-deployment > /dev/null
+# kubectl -n default get deployment devsecops-deployment > /dev/null
 
-if [[ $? -ne 0 ]]; then
-    echo "deployment devsecops-deployment doesn't exist"
+# if [[ $? -ne 0 ]]; then
+#     echo "deployment devsecops-deployment doesn't exist"
+#     kubectl -n default apply -f deployment.yaml
+#     kubectl -n default apply -f service.yaml
+# else
+#     echo "deployment devsecops-deployment exist"
+#     echo "image name - masudrana09/numeric-app:latest"
+#     kubectl -n default set image deploy devsecops-deployment devsecops-container=masudrana09/numeric-app:latest --record=true
+# fi
+
     kubectl -n default apply -f deployment.yaml
     kubectl -n default apply -f service.yaml
-else
-    echo "deployment devsecops-deployment exist"
-    echo "image name - masudrana09/numeric-app:latest"
-    kubectl -n default set image deploy devsecops-deployment devsecops-container=masudrana09/numeric-app:latest --record=true
-fi
-
