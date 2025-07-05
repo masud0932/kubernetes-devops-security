af5359c8b939c6360633a06ddc3208d7a0983b52
masud0932 <153615670+masud0932@users.noreply.github.com>
2025-07-05 10:53:34 +0200

    Add CIS benchmark scripts for Kubelet and Master node checks

---
+++ cis-master.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#cis-master.sh
+
+total_fail=$(kube-bench master --check 1.1.1,1.1.1 --json | jq .[].total_fail)
+
+if [[ "$total_fail" -ne 0 ]];
+        then
+                echo "CIS Benchmark Failed MASTER while testing for 1.1.1,1.1.1"
+                exit 1;
+        else
+                echo "CIS Benchmark Passed for MASTER - 1.1.1,1.1.1"
+fi;
